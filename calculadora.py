898037e16e3a71e277a83bb4b40baecceecbd376
Kold-chan <vicenteperaltaf18@gmail.com>
2026-07-01 13:58:05 -0400

    actualizacion

--- calculadora.py
+++ calculadora.py
@@ -4,3 +4,11 @@ print("2. Resta")
 print("3. Multiplicación")
 print("4. División")
 
+print("Seleccione una operación (1/2/3/4): ")
+opcion = input()
+
+input("Ingrese el primer número: ")
+num1 = float(input())
+
+input("Ingrese el segundo número: ")
+num2 = float(input())
